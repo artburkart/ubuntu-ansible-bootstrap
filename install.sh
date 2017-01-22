99350a50b023480021e43f200d0f55645bd9668e
Arthur Burkart <artburkart@gmail.com>
2017-01-22 01:51:27 -0500

    Adds silver_searcher; Fixes install of sublime,rc

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install roles
-ansible-galaxy install -r roles.yml -p roles
+ansible-galaxy install -r requirements.yml -p roles
 
 # Execute installation of roles defined in `all.yml`
 sudo ansible-playbook -i hosts -b ./env/all.yml
