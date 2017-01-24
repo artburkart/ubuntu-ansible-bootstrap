58d9cb7f5dcdb5946fdf8f3775c4fd907133b046
Arthur Burkart <artburkart@gmail.com>
2017-01-23 22:07:35 -0500

    Fix network-manager fix.
    While making these changes I found a bug in ansible where it still
    performs includes even though a tag has been specified.

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@
 ansible-galaxy install -r requirements.yml -p roles
 
 # Execute installation of roles defined in `all.yml`
-sudo ansible-playbook -i hosts -b ./env/all.yml
+sudo ansible-playbook -i localhost, -c local -b ./env/all.yml
 
